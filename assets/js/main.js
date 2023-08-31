1f7fabcf159c0d64952aba2f76ae7cb9929f37a6
douglas <mdrmatos43@gmail.com>
2023-08-30 23:49:32 -0300

    subindo javas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
-console.log( 'sucesso')
+
+
+const offset = 0
+const limit = 2
+const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}'
+
+fetch(url)
+.then (function (response){
+    console.log(response)
+})
+
+.catch(function(error){
+    console.log(error)
+})
+.finally(function(){
+    console.log('Requesição concluída')
+})
