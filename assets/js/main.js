4641942fa4f41cc708a82d3fa182fd2a5905a967
douglas <mdrmatos43@gmail.com>
2023-08-31 00:19:20 -0300

    subindo modificação JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,38 @@
 
 
 const offset = 0
-const limit = 2
+const limit = 10
 const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}'
 
 fetch(url)
-.then (function (response){
-    console.log(response)
-})
 
-.catch(function(error){
-    console.log(error)
+
+
+
+/* posso usar dessa forma para melhorar a visibilidade do código
+.then (function (response){
+    return response.json()
 })
-.finally(function(){
-    console.log('Requesição concluída')
+.then (function(jsonbody){
+    console.log(jsonbody)
 })
+*/
+
+/* e para melhorar ainda mais posso usar  arrow function, para situação de callback uma sintax reduzida*/
+.then ((response) => response.json())
+.then ((jsonbody) =>    console.log(jsonbody))
+.catch((error) => console.log(error))
+
+
+    /* OU posso utilizar o .then DESSA FORMA!
+    .......................
+    response
+    .json()
+    .then(function (responsebody){
+        console.log(responsebody)
+    })
+})
+.................................
+*/
+
+
